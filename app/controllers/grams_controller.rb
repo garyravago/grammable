cddf2e33159f4c7275bef32aa57f859fdce4f2c4
Gary Ravago <ravagog@gmail.com>
2017-09-07 20:59:20 -0700

    Add Gram edit and update actions along with specs

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -7,13 +7,16 @@ class GramsController < ApplicationController
 
   def show
     @gram = Gram.find_by_id(params[:id])
-    unless @gram
-      render plain: "404 Error. Page not found", status: :not_found
-    end
+    return render_not_found unless @gram
   end
 
   def new
     @gram = Gram.new
+  end
+
+  def edit
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found unless @gram
   end
 
   def create
@@ -25,7 +28,24 @@ class GramsController < ApplicationController
     end
   end
 
+  def update
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found unless @gram
+
+    @gram.update_attributes(gram_params)
+    if @gram.valid?
+      redirect_to root_url
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
+
+  def render_not_found
+    render plain: "404 Error. Page not found", status: :not_found
+  end
+
 
   def gram_params
     params.require(:gram).permit(:message)
